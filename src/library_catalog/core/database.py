7fdf9aa3b22a2bdf6d475fcb4b928c2ab883c901
Pavel <pashaarhipov22@gmail.com>
2026-03-30 15:25:51 +0300

    add session commit in get_db dependency

--- src/library_catalog/core/database.py
+++ src/library_catalog/core/database.py
@@ -22,6 +22,7 @@ async def get_db() -> AsyncGenerator[AsyncSession, None]:
     async with async_session_maker() as session:
         try:
             yield session
+            await session.commit()
         except Exception:
             await session.rollback()
             raise
